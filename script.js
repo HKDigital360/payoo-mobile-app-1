2c2ac398bfe9a9ecceb807079743094a3a8f60bf
HKDigital360 <hkibria528@gmail.com>
2025-12-24 01:23:35 +0600

    js added

---
+++ script.js
@@ -0,0 +1,8 @@
+// login button functionality
+document.getElementById("loginButton").addEventListener("click",function(){
+    console.log("login button click");
+});
+// document.getElementById("loginButton").addEventListener("click", function() { console.log("login button click"); });
+
+
+// console.log("login button click");
