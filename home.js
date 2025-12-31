66b18f56988c7c39bce02825a7992b0b001d0ac5
HKDigital360 <hkibria528@gmail.com>
2025-12-31 11:05:14 +0600

    valid pin & number alert added

--- home.js
+++ home.js
@@ -1,6 +1,17 @@
 document.getElementById("btn-add-money").addEventListener("click",function(e){
     e.preventDefault()
     
+    const accountNumber = document.getElementById("account-number").value
+
+    const pinNumber = document.getElementById("pin-number").value
+
+    if(accountNumber.length = 11 ,  pinNumber.length = 4)
+    {
+        alert("Provide a Valid Account & Pin Number")
+        return;
+        
+    }
+
    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
   
 
