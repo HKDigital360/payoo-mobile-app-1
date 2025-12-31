d60087f5c6a281283bc1415ee1c2f22270ac89f5
HKDigital360 <hkibria528@gmail.com>
2025-12-31 11:42:02 +0600

    alert redefined

--- home.js
+++ home.js
@@ -4,13 +4,22 @@ document.getElementById("btn-add-money").addEventListener("click",function(e){
     const accountNumber = document.getElementById("account-number").value
 
     const pinNumber = document.getElementById("pin-number").value
-
-    if(accountNumber.length = 11 ,  pinNumber.length = 4)
-    {
-        alert("Provide a Valid Account & Pin Number")
-        return;
+if (accountNumber.length !== 11 || pinNumber.length !== 4) { alert("Provide a valid Account Number (11 digits) and Pin Number (4 digits)"); return; }
+    // if(accountNumber.length !== 11)
+    // {
+    //     alert("Provide a Valid Account ")
+    //     return;
         
-    }
+    // }
+
+
+    // if(pinNumber.length !== 4)
+    // {
+    //     alert("Pin Number")
+    //     return;
+        
+    // }
+
 
    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
   
