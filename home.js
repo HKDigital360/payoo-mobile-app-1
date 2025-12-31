3ab4b2d0badb85fc1f7b5d936163c58de6d39ccd
HKDigital360 <hkibria528@gmail.com>
2025-12-31 10:18:02 +0600

    add money & available balance are sum

--- home.js
+++ home.js
@@ -1,1 +1,14 @@
-console.log("home.js")
+document.getElementById("btn-add-money").addEventListener("click",function(e){
+    e.preventDefault()
+    
+   const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+  
+
+   const addAmount = parseInt (document.getElementById("amount-to-add").value)
+
+
+   const newBalance = availableBalance + addAmount
+
+   document.getElementById("available-balance").innerText = newBalance
+   
+})
